606e04e6bcea9603184fe8c6e8432c4f34705957
Swecha Reddy Thoomkunta <142924204+lanonymatestlacle@users.noreply.github.com>
2024-11-19 10:50:30 +0530

    Update and rename pro_1.java to Vignesh-on-Date.java

---
+++ visaa_prep/Vignesh-on-Date.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        long x=s.nextLong();
+        long y=s.nextLong();
+        if(x>=y)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
