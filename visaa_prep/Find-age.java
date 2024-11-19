7ba8a3cc397fb9e5458ce0f18cf45739145d7f08
Swecha Reddy Thoomkunta <142924204+lanonymatestlacle@users.noreply.github.com>
2024-11-19 11:03:50 +0530

    Create Find-age.java

---
+++ visaa_prep/Find-age.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int x=s.nextInt();
+        System.out.println(x-15);
+    }
+}
