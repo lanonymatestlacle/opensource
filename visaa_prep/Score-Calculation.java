db3ce44b9185e9c48d8be3336beb386754a52642
Swecha Reddy Thoomkunta <142924204+lanonymatestlacle@users.noreply.github.com>
2024-11-19 11:46:45 +0530

    Create Score-Calculation.java

---
+++ visaa_prep/Score-Calculation.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<t;i++){
+            int x=s.nextInt();
+            int n=s.nextInt();
+            int m=x/10;
+            System.out.println(m*n);
+        }
+    }
+}
